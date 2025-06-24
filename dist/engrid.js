dfe220f8e0d3bd9ba07a610b103751787e5e0945
bryancasler <bryan.casler@gmail.com>
2025-06-24 16:07:14 -0400

    Corrected mobile background color, hid unused sections, and re-did mobile max-width

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Tuesday, June 24, 2025 @ 15:49:05 ET
+ *  Date: Tuesday, June 24, 2025 @ 16:04:14 ET
  *  By: bryancasler
  *  ENGrid styles: v0.22.4
  *  ENGrid scripts: v0.22.7
